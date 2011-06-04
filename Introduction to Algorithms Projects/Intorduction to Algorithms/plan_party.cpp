32b94d49ed78e07290989c06a1421e20bb43788f
chuanqi.tan@gmail.com <chuanqi.tan@gmail.com@2a65056a-1abf-9332-eb71-4e5b6ffc3986>
2011-06-04 03:36:38 +0000

    写完了“双调旅程”和“聚会计划” 终于可以说我懂了动态规划：寻找最优子问题是一切的关键所在！

---
+++ Introduction to Algorithms Projects/Intorduction to Algorithms/plan_party.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <iomanip>
+#include <limits>
+#include <fstream>
+#include <string>
+#include <numeric>
+#include <set>
+using namespace std;
+
+namespace chapter15
+{
+	namespace
+	{
+		/// 一个人
+		struct Human
+		{
+			vector<Human *> Childs;
+			int				LovePartyValue;
+			string			Name;
+
+			int				IncludeValue;			///< 该Human参与聚会时，以他为根的子树所能得到的最大喜欢度总和
+			int				NotIncludeValue;		///< 该Human不参与聚会时，以他为根的子树所能得到的最大喜欢度总和
+			//IsValid是动态规划的备忘录方式的一个变种
+			bool			IsValid;				///< IncludeValue,NotIncludeValue值是否有效？（无效时为默认值0）
+
+			Human(string const &name, int rank_value) 
+				: Name(name), LovePartyValue(rank_value), IncludeValue(rank_value), IsValid(false)
+			{}
+		};
+
+		void PlanPartyViaDyanmicProgramming(Human *root)
+		{
+			if (root != NULL && !root->IsValid)
+			{
+				//root 参加的情况
+				root->IncludeValue = accumulate(root->Childs.begin(), root->Childs.end(), root->LovePartyValue, [](int acc, Human *human)->int{
+					PlanPartyViaDyanmicProgramming(human);
+					return acc + human->NotIncludeValue;
+				});
+
+				//root 不参加的情况
+				root->NotIncludeValue = accumulate(root->Childs.begin(), root->Childs.end(), 0, [](int acc, Human *human)->int{
+					PlanPartyViaDyanmicProgramming(human);
+					return acc + max(human->IncludeValue, human->NotIncludeValue);
+				});
+
+				root->IsValid = true;
+			}
+
+		}
+	}
+
+	/// 聚会规划
+	void PlanParty()
+	{
+		Human *root = new Human("TCQ", 4);		//总裁结点
+
+		Human *c1 = new Human("WY", 2);
+		Human *c2 = new Human("ABC", 3);
+		root->Childs.push_back(c1);
+		root->Childs.push_back(c2);
+
+		PlanPartyViaDyanmicProgramming(root);
+		cout << max(root->IncludeValue, root->NotIncludeValue);
+	}
+}
