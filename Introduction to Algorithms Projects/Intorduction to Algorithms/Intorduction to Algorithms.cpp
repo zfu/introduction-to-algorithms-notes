c12ed3f5804552d6f938a110ad393856fb445551
chuanqi.tan@gmail.com <chuanqi.tan@gmail.com@2a65056a-1abf-9332-eb71-4e5b6ffc3986>
2011-06-05 13:44:01 +0000

    完成17章的代码（只写了一个huffman编码）。 补完了17章及之前的所有笔记

--- Introduction to Algorithms Projects/Intorduction to Algorithms/Intorduction to Algorithms.cpp
+++ Introduction to Algorithms Projects/Intorduction to Algorithms/Intorduction to Algorithms.cpp
@@ -7,51 +7,54 @@
 
 namespace chapter6
 {
-int test();
+    int test();
 }
 namespace chapter7
 {
-int test();
+    int test();
 }
 namespace chapter8
 {
-int test();
+    int test();
 }
 namespace chapter9
 {
-int test();
+    int test();
 }
 namespace chapter11
 {
-int test();
+    int test();
 }
 namespace chapter12
 {
-int test();
+    int test();
 }
 namespace chapter13
 {
-int test();
+    int test();
 }
 namespace chapter15
-{//动态规划
-
-void AssembleDispatch();				//装配线调度
-void MatrixListMultiply();				//矩阵链乘法
-int LongestCommonSubsequence();			//最长公共子序列
-void BestBinarySearchTree();			//最优二叉查找树
-void NeatlyPrint();						//整齐打印
-void PlanParty();						//聚会计划
-void BitonicTour();						//双调旅程
+{
+    //动态规划
+    void AssembleDispatch();				//装配线调度
+    void MatrixListMultiply();				//矩阵链乘法
+    int LongestCommonSubsequence();			//最长公共子序列
+    void BestBinarySearchTree();			//最优二叉查找树
+    void NeatlyPrint();						//整齐打印
+    void PlanParty();						//聚会计划
+    void BitonicTour();						//双调旅程
 
 }
-
+namespace chapter16
+{
+	//贪心算法
+    void HuffmanCode();						//哈夫曼编码
+}
 
 int _tmain( int argc, _TCHAR* argv[] )
 {
-    //chapter13::test();
-    chapter15::BitonicTour();
-	int a[2][2] ={{1,2},{2,3}};
+    chapter16::HuffmanCode();
+    int a[2][2] = {{1, 2}, {2, 3}};
     return 0;
 }
 
---
+++ Introduction to Algorithms Projects/Intorduction to Algorithms/huffman_code.cpp
@@ -0,0 +1,94 @@
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <iterator>
+#include <iomanip>
+#include <limits>
+#include <fstream>
+#include <string>
+#include <numeric>
+#include <set>
+#include <map>
+using namespace std;
+
+namespace chapter16
+{
+    namespace
+    {
+        struct Node
+        {
+            Node *Left, *Right;
+            char	Char;
+            double	Value;
+        };
+
+        void ShowLeaf( Node * root, string const &code )
+        {
+            if ( root )
+            {
+                if ( !root->Left && !root->Right )
+                {
+                    //叶子结点了
+                    cout << root->Char << " ===> " << code << endl;
+                }
+                else
+                {
+                    ShowLeaf( root->Left, code + "0" );
+                    ShowLeaf( root->Right, code + "1" );
+                }
+            }
+        }
+    }
+
+    /// 使用贪心算法解决Huffman编码问题
+    void HuffmanCode()
+    {
+        string orgin_str = "abcadbabacadbacdabadeabadabaewbadsafdsawebadsfasdfababadasdfabababcadb";
+        map<char, double> frequency;
+        for_each( orgin_str.begin(), orgin_str.end(), [&]( char const & c )
+        {
+            ++frequency[c];
+        } );
+
+        vector<Node *> nodes;
+        for_each( frequency.begin(), frequency.end(), [&]( pair<char const, double> &item )
+        {
+            item.second /= orgin_str.size();
+
+            Node *node = new Node();
+            node->Value = item.second;
+            node->Char = item.first;
+            nodes.push_back( node );
+        } );
+
+        for_each( frequency.begin(), frequency.end(), [&]( pair<char const, double> &item )
+        {
+            cout << item.first << ":  " << item.second << endl;
+        } );
+
+        while ( nodes.size() > 1 )
+        {
+            //有超过一个结点，则循环
+            sort( nodes.begin(), nodes.end(), []( Node * n1, Node * n2 )
+            {
+                return n1->Value < n2->Value;
+            } );
+
+            Node *new_node = new Node();
+            new_node->Left = nodes[0];
+            new_node->Right = nodes[1];
+            new_node->Value = nodes[0]->Value + nodes[1]->Value;
+
+            nodes.erase( nodes.begin(), nodes.begin() + 2 );
+            nodes.push_back( new_node );
+        }
+
+        ShowLeaf( nodes[0], "" );
+        getchar();
+    }
+
+
+
+
+}
